57a198a5bfa47dadd89f4098e2b2d9a078eb9d6a
Deep Patel <deepkalpana1@aol.com>
2023-12-07 15:32:04 -0500

    finished profile page of settings

--- 490Project/SettingsScreen.js
+++ 490Project/SettingsScreen.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import styles from './settingsStyles';
 
 function SelectProfile() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile</Text>
+    <View style={{ flex: 1, flexDirection: 'column', padding: 20 }}>
+      <Text style={styles.title}>Profile</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ width: 100, alignItems: 'flex-start' }}>
+          <Text style={styles.label}>Address:</Text>
+        </View>
+        <Text style={styles.text}>
+          123 Main Street
+          {'\n'}
+          Columbia, South Carolina
+          {'\n'}
+          29201
+        </Text>
+      </View>
+      <View style={{ flexDirection: 'row', marginTop: 20 }}>
+        <View style={{ width: 100, alignItems: 'flex-start' }}>
+          <Text style={styles.label}>Mobile:</Text>
+        </View>
+        <Text style={styles.text}>(123) 456-7890</Text>
+      </View>
+      <View style={{ flexDirection: 'row', marginTop: 20 }}>
+        <View style={{ width: 100, alignItems: 'flex-start' }}>
+          <Text style={styles.label}>Email:</Text>
+        </View>
+        <Text style={styles.text}>nolammoore@email.com</Text>
+      </View>
     </View>
   );
 }
+
+
 
 function SelectAccount() {
   return (
---
+++ 490Project/settingsStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  title: {
+    marginBottom: 30,
+    fontSize: 35,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
