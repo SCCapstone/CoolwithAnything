429f3eafa59e56fc93ee20c784cbac558204a1e0
Nicholas Rubino <nrubino@email.sc.edu>
2023-12-09 06:48:47 -0500

    fixed calendar functionality

---
+++ 490Project/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainNavigator from './navigators/MainNavigator'; // Adjust the path according to your project structure
+
+const App = () => {
+  return <MainNavigator />;
+};
+
+export default App;
