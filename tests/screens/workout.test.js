c9067f78ede1d7c0d486444f5bcfb7b7cbe61ea4
Kayly Tran <ktran7895@gmail.com>
2024-01-28 23:36:56 -0500

    update

---
+++ tests/screens/workout.test.js
@@ -0,0 +1,14 @@
+// MyComponent.test.js
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import WorkoutScreen from '../../screens/WorkoutScreen';
+
+describe('WorkoutScreen', () => {
+  it('renders correctly', () => {
+    render(<WorkoutScreen />);
+    // Use queries from @testing-library/react to assert on the rendered component
+    expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+  });
+});
