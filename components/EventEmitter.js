9aff2a687089bf6d736db17e445ea371060161dd
Kayly Tran <ktran7895@gmail.com>
2024-02-26 12:02:00 -0500

    wip

--- components/EventEmitter.js
+++ components/EventEmitter.js
@@ -1,4 +1,34 @@
 class EventEmitter {
+<<<<<<< HEAD
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(eventName, fn) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(fn);
+
+    return () => {
+      this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
+    };
+  }
+
+  emit(eventName, data) {
+    const event = this.events[eventName];
+    if (event) {
+      event.forEach(fn => {
+        fn.call(null, data);
+      });
+    }
+  }
+}
+
+const eventEmitter = new EventEmitter();
+export default eventEmitter;
+=======
     constructor() {
       this.events = {};
     }
@@ -27,3 +57,4 @@ class EventEmitter {
   
   const eventEmitter = new EventEmitter();
   export default eventEmitter;
+>>>>>>> d14dd996 (fixed calendar refresh and added priority for task)
