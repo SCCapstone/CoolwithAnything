1afb9c46d0ed0d2dbd83ef0648afe61b0d1d2012
Deep Patel <deepkalpana1@aol.com>
2024-04-22 12:16:33 -0400

    removed previous detox test history to free space

--- e2e/addMeal.test.js
+++ e2e/addMeal.test.js
@@ -16,11 +16,6 @@ describe('Add meal test', () => {
       await device.launchApp({newInstance: true});
   });
 
-  afterEach(async () => {
-    //await detox.cleanup();
-    await device.terminateApp();
-  });
-
   it('should add meal', async () => {
       await loginUser();
       console.log("Logged in");
@@ -38,12 +33,12 @@ describe('Add meal test', () => {
       await waitFor(element(by.id('add-meal-test'))).toBeVisible().withTimeout(TIMEOUT);
       console.log('Add meal screen open');
       //await element(by.id('meal-name')).tap();
-      await tapVisibleText('meal-name', 'Tacos');
-      await tapVisibleText('meal-ingredients', 'Tortillas, Beef, Cheese');
+      await element(by.id('meal-name')).typeText('Tacos');
+      await element(by.id('meal-ingredients')).typeText('Tortilla, Beef, Cheese');
       await tapVisibleText('meal-servings', '4');
       await tapVisibleText('meal-instructions', 'Assemble and eat');
       await device.pressBack();
       await tapVisibleButton('submit-meal');
       await element(by.text("CREATE")).tap();
-    });
+  });
 });
